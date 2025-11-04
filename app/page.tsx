08fe9ce6eea49b73c056f84871dae51867baf2f3
mrr0pr0 <robert.talberg@icloud.com>
2025-11-04 15:20:22 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -37,7 +37,7 @@ export default function Home() {
             {" "}center.
           </p>
           <div className="mt-2">
-            <Link href="/first-page">
+            <Link href="./firstpage">
               <button className="rounded bg-foreground px-3 py-1 text-background">
                 Open first page
               </button>
